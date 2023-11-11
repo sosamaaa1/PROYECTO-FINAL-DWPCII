accf5dfafa654b4d2acea6a3fef1f7f9356a7a66
sosamaaa1 <126584200+sosamaaa1@users.noreply.github.com>
2023-11-11 07:18:32 +0000

    ✨Corrigiendo errores

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ const port = 3000;
 
 // Configurar Handlebars como el motor de vistas
 const exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+
+app.engine('.hbs', exphbs({ extname: '.hbs', defaultLayout: 'main', layoutsDir: __dirname + '/views/layouts/' }));
+app.set('view engine', '.hbs');
 
 // Servir archivos estáticos desde la carpeta "public"
 app.use(express.static('public'));
 
 // Definir una ruta inicial
 app.get('/', (req, res) => {
-  res.render('index'); // Renderizar la vista llamada "index.handlebars"
+  res.render('index'); // Renderizar la vista llamada "index.hbs"
 });
 
 app.listen(port, () => {
