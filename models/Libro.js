193a2020f192c5292639c6e10d33793b368d9666
sosamaaa1 <126584200+sosamaaa1@users.noreply.github.com>
2023-11-12 22:28:24 +0000

    ✨ Agregando estilos

--- models/Libro.js
+++ models/Libro.js
@@ -5,6 +5,22 @@ const libroSchema = new mongoose.Schema({
     titulo: {
         type: String,
         required: true
+    }, 
+    autor: {
+        type: String,
+        required: true
+    },
+    categoria: {
+        type: String,
+        required: true
+    },
+    ISBN: {
+        type: String,
+        required: true
+    },
+    copiasDisponibles: {
+        type: Number,  // Cambiado a Number si la cantidad es un número entero
+        required: true
     },
 });
 
