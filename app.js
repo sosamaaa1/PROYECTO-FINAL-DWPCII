c674f04afa174ef394543f1970297328db3a67ab
sosamaaa1 <126584200+sosamaaa1@users.noreply.github.com>
2023-11-24 09:15:56 +0000

    ✨ Cargando estilos

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
 const bodyParser = require('body-parser');
 const Libro = require('./models/Libro');
@@ -29,7 +30,8 @@ app.engine('.hbs', exphbs({
 app.set('view engine', '.hbs');
 
 // Servir archivos estáticos desde la carpeta "public"
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Definir una ruta inicial
 app.get('/', (req, res) => {
